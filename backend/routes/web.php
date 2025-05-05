bf82ae6583e51c10e32bc85d58531d0c4e53d19c
amhar4u <muhammedamhar@gmail.com>
2025-05-05 18:56:40 +0530

    Remove the logout controller

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\LogoutController;
+
 use App\Livewire\Login;
 use App\Livewire\Admin\Users;
 use App\Livewire\Admin\Products;
@@ -26,7 +26,6 @@ use App\Livewire\Admin\AdminDashboard;
 Route::get('/',Login::class)->name('login');
 
 Route::middleware(['auth','role:admin'])->group(function () {
-    Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
     Route::get('/admin/dashboard', AdminDashboard::class)->name('admin.dashboard');
     Route::get('/admin/products', Products::class)->name('admin.products');
     Route::get('/admin/users', Users::class)->name('admin.users');
